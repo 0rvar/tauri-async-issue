5d52c23de93fdc83f7e4454fccc192a094bf5d50
Orvar Segerström <orvarsegerstrom@gmail.com>
2022-03-31 18:05:02 +0200

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,26 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+#![feature(async_closure)]
+
+fn main() {
+    let app = tauri::Builder::default()
+        .build(tauri::generate_context!())
+        .expect("failed to build Tauri application");
+
+    let server = actix_web::HttpServer::new(move || {
+        actix_web::App::new().route(
+            "/{token}",
+            actix_web::web::get().to(async || "Hello, world"),
+        )
+    })
+    .bind("127.0.0.1:5005")
+    .expect("Failed to run http server")
+    .run();
+    tauri::async_runtime::spawn(server);
+
+    app.run(|_app_handle, event| match event {
+        _ => {}
+    });
+}
